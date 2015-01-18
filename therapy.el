9d1c18c2dbbd5649631ba15b42ff38c1952012d6
Austin Bingham <austin.bingham@gmail.com>
2015-01-18 16:31:34 +0100

    Boilerplate updates.

--- therapy.el
+++ therapy.el
@@ -1,9 +1,44 @@
-;;; therapy --- Useful Python stuff
-
+;;; therapy.el --- Useful Python stuff
+;;
+;; Copyright (c) 2015 Austin Bingham
+;;
+;; Author: Austin Bingham <austin.bingham@gmail.com>
+;; Version: 0.1
+;; URL: https://github.com/abingham/therapy
+;; Package-Requires: ((emacs "24") (dash "2.10.0") (f "0.17.2"))
+;;
+;; This file is not part of GNU Emacs.
+;;
 ;;; Commentary:
 ;;
 ;; "This is supposed to be torture, not therapy!"  -- Minerva Mayflower
 ;;
+;; Description:
+;;
+;; therapy is a suite of functionality that improves Python
+;; development in Emacs.
+;;
+;;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Code:
 
